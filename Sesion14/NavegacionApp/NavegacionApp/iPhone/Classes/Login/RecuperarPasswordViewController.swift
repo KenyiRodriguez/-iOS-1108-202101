0f131228ff7c3dffc185d9093471b5250f5cb9b5
Kenyi Rodriguez <krodriguez@solera.pe>
2021-07-17 22:31:51 -0500

    --

---
+++ Sesion14/NavegacionApp/NavegacionApp/iPhone/Classes/Login/RecuperarPasswordViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecuperarPasswordViewController.swift
+//  NavegacionApp
+//
+//  Created by Kenyi Rodriguez on 12/07/21.
+//
+
+import UIKit
+
+class RecuperarPasswordViewController: UIViewController {
+    
+    @IBAction func clickBtnBack(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
---
+++ Sesion14/NavegacionApp/NavegacionApp/iPhone/Classes/Productos/DetalleProductoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetalleProductoViewController.swift
+//  NavegacionApp
+//
+//  Created by Kenyi Rodriguez on 12/07/21.
+//
+
+import UIKit
+
+class DetalleProductoViewController: UIViewController {
+    
+    @IBAction func clickBtnBack(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
